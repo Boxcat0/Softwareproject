6ebb61d16cc4a9867db7444368787c8807cb4b70
mundo99 <ansehdus7@gmail.com>
2023-05-31 16:02:42 +0900

    Models추가

--- Back-end/src/main/kotlin/com/example/backend/models/Models.kt
+++ Back-end/src/main/kotlin/com/example/backend/models/Models.kt
@@ -1,6 +1,7 @@
 package com.example.backend.models
 
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 import java.util.*
 
 @Document("Member")
@@ -27,3 +28,11 @@ data class Trainer(
     val gymplace: String = "",
 )
 
+@Document("Review")
+data class Review(
+    var userId: String = "",
+    val gym: String = "",
+    var star: Int = 0,
+    val reviews: String = "",
+    val date: LocalDate = LocalDate.now(),
+)
