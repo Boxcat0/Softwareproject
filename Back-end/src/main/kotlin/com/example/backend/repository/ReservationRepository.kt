f99714bb4117e29c2b0d43a8819f6c2ab4e77233
mundo99 <ansehdus7@gmail.com>
2023-06-02 15:26:51 +0900

    예약DB 저장

--- Back-end/src/main/kotlin/com/example/backend/repository/ReservationRepository.kt
+++ Back-end/src/main/kotlin/com/example/backend/repository/ReservationRepository.kt
@@ -1,13 +1,12 @@
 package com.example.backend.repository
 
+import com.example.backend.models.Reservation
 import com.example.backend.models.Review
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.stereotype.Repository
 
-
 @EnableMongoRepositories
 @Repository
-interface ReviewRepository : MongoRepository<Review, Long> {
-
+interface ReservationRepository : MongoRepository<Reservation, String> {
 }
